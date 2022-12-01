07a8071ce4c4515b3fdd657306dfc97e78d4d8fb
johnnyliu <bakolama@gmail.com>
2022-12-01 18:46:05 +0800

    run first page and parse data
    run `scrapy crawl stock_rank` you will see result.

--- scrapy_tutor101/spiders/stock_rank.py
+++ scrapy_tutor101/spiders/stock_rank.py
@@ -2,9 +2,31 @@ import scrapy
 
 
 class StockRankSpider(scrapy.Spider):
-    name = 'stock_rank'
-    allowed_domains = ['histock.tw/stock/rank.aspx']
-    start_urls = ['http://histock.tw/stock/rank.aspx/']
+    name = "stock_rank"
+    allowed_domains = ["histock.tw/stock/rank.aspx"]
+    start_urls = ["http://histock.tw/stock/rank.aspx/"]
 
     def parse(self, response):
-        pass
+        selectors = response.selector.xpath(
+            '//table[@class="gvTB"]/tr[position()>1]'
+        )  # get selector list
+        result = [
+            (
+                tr.xpath("td[1]/text()").get(),
+                tr.xpath("td[2]/a/text()").get(),
+                tr.xpath("td[3]/span/text()").get(),
+            )
+            for tr in selectors
+        ]
+
+        css_selectors = response.css("table.gvTB>tr")[1:-1]
+        css_result = [
+            (
+                tr.css("td:first-child::text").get(),
+                tr.css("td:nth-child(2)>a::text").get(),
+                tr.css("td:nth-child(3)>span::text").get(),
+            )
+            for tr in css_selectors
+        ]
+
+        yield {"result": result, "css_result": css_result}
